a09cedcc19b4aa7607e1f222f66abcea828914b8
Fanilo Andrianasolo <andfanilo@gmail.com>
2020-06-21 19:01:42 +0200

    :books: Edit gif demo and app

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ b_color = st.sidebar.beta_color_picker("Enter brush color hex: ")
 bg_color = st.sidebar.beta_color_picker("Enter background color hex: ", "#eee")
 drawing_mode = st.sidebar.checkbox("Drawing mode ?", True)
 image_data = st_canvas(
-    b_width, b_color, bg_color, drawing_mode=drawing_mode, key="canvas"
+    b_width, b_color, bg_color, height=150, drawing_mode=drawing_mode, key="canvas"
 )
 if image_data is not None:
     st.markdown(f"Displaying numpy data with imshow :")
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12, 4))
     ax.imshow(image_data)
     time.sleep(0.2)  # <-- temporary because MFM
     st.pyplot(fig, clear_figure=False)
