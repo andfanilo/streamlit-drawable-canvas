af25c79a22422d4360cf1c48ef4657dd91dd4bde
Fanilo Andrianasolo <andfanilo@gmail.com>
2020-07-06 19:11:19 +0200

    Bump to version 0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme() -> str:
 
 setuptools.setup(
     name="streamlit-drawable-canvas",
-    version="0.0.1",
+    version="0.1.0",
     author="Fanilo ANDRIANASOLO",
     author_email="andfanilo@gmail.com",
     description="A Streamlit custom component for a free drawing canvas using Fabric.js.",
