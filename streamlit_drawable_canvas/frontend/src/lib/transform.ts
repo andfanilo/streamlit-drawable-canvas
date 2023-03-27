8fdca81c380ea0d5c28a9e743609aecf8fba1987
Diana Mkhitaryan <diana@scopeo.ai>
2023-03-27 12:27:35 +0200

    modification of transformer mode, new application selecting bounding boxes for data curation

--- streamlit_drawable_canvas/frontend/src/lib/transform.ts
+++ streamlit_drawable_canvas/frontend/src/lib/transform.ts
@@ -5,17 +5,19 @@ class TransformTool extends FabricTool {
     let canvas = this._canvas
     canvas.isDrawingMode = false
     canvas.selection = true
-    canvas.forEachObject((o) => (o.selectable = o.evented = true))
+    canvas.forEachObject((o) => (o.selectable = o.evented = o.lockMovementX = o.lockMovementY =true))
+    canvas.forEachObject((o) => (o.hasControls =false))
 
-    // instead of looking for target of double click,
-    // assume double click on object clears the selected object
-    const handleDoubleClick = () => {
-      canvas.remove(canvas.getActiveObject())
-    }
-
-    canvas.on("mouse:dblclick", handleDoubleClick)
+    const handleClick = () => {
+      const activeObject = canvas.getActiveObject()
+      if (activeObject && activeObject.selectable) {
+        activeObject.set({ fill: 'rgb(208, 240, 192, 0.2)', stroke: 'rgb(50,205,50)'})
+        canvas.renderAll()
+  }
+}
+    canvas.on("mouse:down", handleClick)
     return () => {
-      canvas.off("mouse:dblclick", handleDoubleClick)
+      canvas.off("mouse:down", handleClick)
     }
   }
 }
